194b53e889b0daa7fef7b175d2da96d8520bf686
Jieshao <1806620741@qq.com>
2019-08-24 21:57:03 +0800

    add linux shell

---
+++ gobash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export GOPATH=$PWD
+export GOBIN=$PWD/bin
+export PATH=$PATH:$GOBIN
+echo "**************CURRENT**************"
+echo GOPATH=$GOPATH
+echo GOBIN=$GOBIN
+echo "***********************************"
+echo "Now you are in Gosh, Do your need!"
+bash
---
+++ goenv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export GOPATH=$PWD
+export GOBIN=$PWD/bin
+export PATH=$PATH:$GOBIN
+export PS1="GOSH:\w\>"
+echo "**************CURRENT**************"
+echo GOPATH=$GOPATH
+echo GOBIN=$GOBIN
+echo "***********************************"
+echo "Now you are in Gosh, Do your need!"
